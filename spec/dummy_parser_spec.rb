5541b3cd1c7c412cba6e1e685133fdefd4c27c3a
coaxial <hi@64b.it>
2019-11-20 15:10:20 +0100

    Add third transaction to test statement_{from,to}

--- spec/dummy_parser_spec.rb
+++ spec/dummy_parser_spec.rb
@@ -29,6 +29,7 @@ RSpec.describe(Parser::Dummy) do
       expected = <<~ROWS
         "Date","Payee","Memo","Outflow","Inflow"
         "23/12/2019","coaxial","","1000000.00",""
+        "30/12/2019","Santa","","50000.00",""
         "02/02/2020","Someone Else","","45.00",""
       ROWS
 
