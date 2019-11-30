f0bcfba207d4f4c9c57d28020085b75c13c4e345
coaxial <hi@64b.it>
2019-11-30 20:14:36 +0100

    Use example processor

--- spec/example_processor_spec.rb
+++ spec/example_processor_spec.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
-require 'helpers/dummy_processor.rb'
-
-RSpec.describe(Processor::Dummy) do
+RSpec.describe(Processor::Example) do
   context('with any file') do
     before(:context) do
-      @subject = Processor::Dummy.new(file: File.join(File.dirname(__FILE__),
-                                                      'fixtures/valid.csv'))
+      @subject = Processor::Example.new(file: File.join(File.dirname(__FILE__),
+                                                        'fixtures/valid.csv'))
     end
 
     it 'instantiates' do
-      expect(@subject).to be_an_instance_of(Processor::Dummy)
+      expect(@subject).to be_an_instance_of(Processor::Example)
     end
 
     it 'inherits from Processor::Base' do
@@ -21,12 +19,12 @@ RSpec.describe(Processor::Dummy) do
   context 'with a valid CSV file' do
     before(:context) do
       filename = File.join(File.dirname(__FILE__), 'fixtures/valid.csv')
-      @subject = Processor::Dummy.new(file: filename)
+      @subject = Processor::Example.new(file: filename)
     end
 
     it 'outputs valid YNAB4 CSV data', :writes_csv do
       @subject.to_ynab!
-      actual = File.read('valid_dummy_bank_20191223-20200202_ynab4.csv')
+      actual = File.read('valid_example_bank_20191223-20200202_ynab4.csv')
       expected = <<~ROWS
         "Date","Payee","Memo","Outflow","Inflow"
         "23/12/2019","coaxial","","1000000.00",""
--- spec/ynab_convert_spec.rb
+++ spec/ynab_convert_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'helpers/dummy_processor.rb'
-
 RSpec.describe YnabConvert do
   it 'has a version number' do
     expect(YnabConvert::VERSION).not_to be nil
@@ -47,13 +45,13 @@ RSpec.describe YnabConvert do
       context 'that is valid CSV' do
         before(:example) do
           filename = File.join(File.dirname(__FILE__), 'fixtures/valid.csv')
-          opts = { file: filename, processor: Processor::Dummy }
+          opts = { file: filename, processor: Processor::Example }
           @subject = YnabConvert::File.new opts
         end
 
         it 'converts it', :writes_csv do
           @subject.to_ynab!
-          actual = File.read('valid_dummy_bank_20191223-20200202_ynab4.csv')
+          actual = File.read('valid_example_bank_20191223-20200202_ynab4.csv')
           expected = <<~ROWS
             "Date","Payee","Memo","Outflow","Inflow"
             "23/12/2019","coaxial","","1000000.00",""
@@ -69,7 +67,7 @@ RSpec.describe YnabConvert do
         before(:example) do
           filename = File.join(File.dirname(__FILE__),
                                'fixtures/not_a_csv_file.txt')
-          opts = { file: filename, processor: Processor::Dummy }
+          opts = { file: filename, processor: Processor::Example }
           @subject = -> { YnabConvert::File.new(opts) }
         end
 
@@ -81,7 +79,7 @@ RSpec.describe YnabConvert do
         it 'cleans up the temporary CSV file' do
           @subject.call.to_ynab!
         rescue RuntimeError
-          expect(Dir['not_a_csv_file.txt_dummy_bank_*.csv'].any?).to be false
+          expect(Dir['not_a_csv_file.txt_example_bank_*.csv'].any?).to be false
         end
       end
     end
