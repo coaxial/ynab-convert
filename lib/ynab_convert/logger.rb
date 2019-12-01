fb608224ec47aefa07d21b5e492fdb223fd4fc93
coaxial <hi@64b.it>
2019-12-01 14:05:48 +0100

    Memoize logger, log FATAL and above by default

--- lib/ynab_convert/logger.rb
+++ lib/ynab_convert/logger.rb
@@ -5,8 +5,9 @@ require 'logger'
 # Add logging to classes
 module YnabLogger
   def logger
+    @logger unless @logger.nil?
     @logger ||= Logger.new(STDERR)
-    @logger.level = Logger::FATAL if ENV['YNAB_CONVERT_ENV'] == 'test'
+    @logger.level = Logger::FATAL
     @logger.level = Logger::DEBUG if ENV['YNAB_CONVERT_DEBUG'] == 'true'
     @logger
   end
