975d3bee79def2e40d0f1a936c7f6a1f5cf2d668
coaxial <hi@64b.it>
2021-02-23 17:58:56 +0100

    Use dynamic year in test

--- spec/revolut_processor_spec.rb
+++ spec/revolut_processor_spec.rb
@@ -27,22 +27,26 @@ RSpec.describe(Processor::Revolut) do
     # it 'outputs valid YNAB4 CSV data', :writes_csv do
     it 'outputs valid YNAB4 CSV data', :writes_csv do
       @subject.to_ynab!
+      # Revolut statements don't include the year. When parsing the date, it
+      # will assume current year. This will change each year, so it must be
+      # computed when the test is executed.
+      current_year = Date.today.year
       actual = File.read(
-        'statement_revolut_20191102-20191206_ynab4.csv'
+        "statement_revolut_#{current_year}1102-#{current_year}1206_ynab4.csv"
       )
       expected = <<~ROWS
         "Date","Payee","Memo","Outflow","Inflow"
-        "06/12/2019","Auto Top-Up by *1234","","","100.00"
-        "05/12/2019","To Bruce Wayne","","20.84",""
-        "02/12/2019","Rigmarole Ltd.","","1106.72",""
-        "02/12/2019","Top-Up by *1234","","","1100.00"
-        "02/12/2019","Auto Top-Up by *1234","","","100.00"
-        "02/12/2019","Exchanged to EUR","","153.14",""
-        "02/12/2019","Top-Up by *1234","","","50.00"
-        "06/11/2019","Payee 3326","","2.53",""
-        "04/11/2019","Payee 391","","58.68",""
-        "04/11/2019","Top-Up by *1234","","","50.00"
-        "02/11/2019","Top-Up by *1234","","","50.00"
+        "06/12/#{current_year}","Auto Top-Up by *1234","","","100.00"
+        "05/12/#{current_year}","To Bruce Wayne","","20.84",""
+        "02/12/#{current_year}","Rigmarole Ltd.","","1106.72",""
+        "02/12/#{current_year}","Top-Up by *1234","","","1100.00"
+        "02/12/#{current_year}","Auto Top-Up by *1234","","","100.00"
+        "02/12/#{current_year}","Exchanged to EUR","","153.14",""
+        "02/12/#{current_year}","Top-Up by *1234","","","50.00"
+        "06/11/#{current_year}","Payee 3326","","2.53",""
+        "04/11/#{current_year}","Payee 391","","58.68",""
+        "04/11/#{current_year}","Top-Up by *1234","","","50.00"
+        "02/11/#{current_year}","Top-Up by *1234","","","50.00"
       ROWS
 
       expect(actual).to eq(expected)
