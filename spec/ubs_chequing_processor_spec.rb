eb48ef9d4f90f8d185551988517b45265b78a6fa
Rock <coaxial@users.noreply.github.com>
2022-03-06 11:48:40 +0100

    [UBS Chequing] Remove junk data from transaction description (#22)
    * Remove junk data from UBS Chequing desc
    
    * Increase version

--- spec/ubs_chequing_processor_spec.rb
+++ spec/ubs_chequing_processor_spec.rb
@@ -33,13 +33,16 @@ RSpec.describe(Processor::UbsChequing) do
       )
       expected = <<~ROWS
         "Date","Payee","Memo","Outflow","Inflow"
-        "06/11/2019","Recouvrement VIS1W OBJECTION TO UBS WITHIN 30 DAYS, UBS SWITZERLAND AG, C/O UBS CARD CENTER, CREDIT CARD STATEMENT, OF 15.11.2019, ACCOUNT NUMBER 0000 1234 5678 9012, LSV débit CHF","","10959.40",""
+        "06/11/2019","Recouvrement VIS1W OBJECTION TO UBS WITHIN 30 DAYS, UBS SWITZERLAND AG, C/O UBS CARD CENTER, CREDIT CARD STATEMENT","","10959.40",""
         "04/11/2019","Ordre e-banking TRANSFER CH0123456789012345678, FRAU MACKENZIE EXAMPLE U/O, HERR WALTER WHITE, E-Banking virement compte à compte","","21502.00",""
         "29/10/2019","Ordre e-banking PAYMENT FRAU MACKENZIE EXAMPLE U/O, HERR WALTER WHITE, PAYMENT, E-Banking virement compte à compte","","1725.00",""
         "29/10/2019","Ordre e-banking PAYMENT FRAU MACKENZIE EXAMPLE U/O, HERR WALTER WHITE, E-Banking virement compte à compte","","920.53",""
         "25/10/2019","Entrée salaire Entrée paiement SIC ","","","16399.80"
         "21/10/2019","Ordre e-banking A STORE 8001 ZURICH, E-Banking CHF intérieur","","175.40",""
         "18/10/2019","Virement WALTER WHITE BAHNHOFSTRASSE 1, 8001 ZURICH, SOME REFERENCE, SECOND LINE, Entrée paiement SIC","","","53512.00"
+        "18/10/2019","Paiement carte de debit CARD 00000000-0 0000 Shop AG, 1234 St. Santis","","3010.66",""
+        "18/10/2019","Ordre PayNet EBILL INVOICE SWISSCOM SCHWEIZ AG, 3050 BERN","","1620.00",""
+        "18/10/2019","Ordre e-banking DO IT GARDEN, MIGROS-GENOSSENSCHAFT S-BUND","","199.10",""
       ROWS
 
       expect(actual).to eq(expected)
