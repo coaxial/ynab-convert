70fa478f51811cbd36a7c8c80449283f4b785986
coaxial <c@64b.it>
2022-03-09 10:53:10 +0100

    Document class

--- lib/ynab_convert/api_client/currency_api.rb
+++ lib/ynab_convert/api_client/currency_api.rb
@@ -19,9 +19,11 @@ module YnabConvert
         }
       end
 
-      # @option base_currency [Symbol] ISO base currency (case insensitive)
-      # @option date [String] Day on which to retrieve rates, formatted as
+      # Fetches historical exchange rate for base_currency on date.
+      # @param base_currency [Symbol] ISO base currency (case insensitive)
+      # @param date [String] Day on which to retrieve rates, formatted as
       #   YYYY-MM-DD
+      # @return [Hash] Exchanges rates for base_currency on date
       def historical(base_currency:, date:)
         handle_date_out_of_bounds(date) if out_of_bounds?(date)
         endpoint = "#{date}/currencies/#{base_currency}.min.json"
@@ -30,6 +32,11 @@ module YnabConvert
 
       private
 
+      # The currency-api only has rates since 2020-11-22 and until yesterday
+      # (the current day's rate are updated at 23:59 on that day). This method
+      # ensures the requested date falls within the available range.
+      # @param date [String] Date in YYYY-MM-DD format
+      # @return [Boolean] Whether the date is out of bounds for this API
       def out_of_bounds?(date)
         parsed_date = Date.parse(date)
 
@@ -37,6 +44,8 @@ module YnabConvert
           parsed_date > @available_date_range[:max]
       end
 
+      # @param date [String] Date in YYYY-MM-DD format
+      # @return [Errno::EDOM] Raises an Errno::EDOM
       def handle_date_out_of_bounds(date)
         error_message = "#{date} is out of the currency-api available date "\
         "range (#{@available_date_range[:min]}–#{@available_date_range[:max]})"
@@ -44,6 +53,8 @@ module YnabConvert
         raise Errno::EDOM, error_message
       end
 
+      # @param [String] The endpoint to query, without a leading `/`
+      # @return [Hash] Exchanges rates for base_currency on date
       def make_request(endpoint:)
         uri = URI(URI.join(@api_base_path, endpoint))
 
