1de4ac7d88039d8592201ac78d69c875085ad81d
coaxial <c@64b.it>
2022-03-14 17:10:15 +0100

    Delete Converters (moved to Processor)

--- lib/ynab_convert/transformers/transformers.rb
+++ lib/ynab_convert/transformers/transformers.rb
@@ -3,7 +3,7 @@
 # Regroups all the classes involved in transforming a given Statement into a
 # YNAB4File
 module Transformers
-  transformers = %w[cleaners converters enhancers formatters validators]
+  transformers = %w[cleaners enhancers formatters validators]
 
   # Load all known Transformers
   transformers.each do |transformer|
